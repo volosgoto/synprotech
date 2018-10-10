deb2f0e9f6f23006e1f4e569ce4c771db928bd5e
volosgoto <volosgoto@yahoo.com>
2018-10-10 15:04:46 +0300

    add SendMail.js add validation methods and phone field

--- app/src/js/SendMail.js
+++ app/src/js/SendMail.js
@@ -33,22 +33,56 @@
 //   }
 // }
 
-// class Validator{
+class Validator{
+  validateEmail() {
+    let email = document.getElementById('email');
+    console.log(email);
+    // let re = /^([a-zA-Z_\-\.]+)@([a-zA-Z_\-\.]+)\.([a-zA-Z]{2,5})$/;
+    let re = /^([\w\-\.]+)@([\w\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+    if (!re.test(email.value)) {
+        email.classList.add('is-invalid');
+    } else {
+        email.classList.remove('is-invalid');
+    }
+  }
+
+   validatePhone() {
+    let phone = document.getElementById('phone');
+    let re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
+ 
+    if (!re.test(phone.value)) {
+        phone.classList.add('is-invalid');
+    } else {
+        phone.classList.remove('is-invalid');
+    }
+}
 
 
+  validateName() {
+    let name = document.getElementById('name');
+    console.log(name);
+    let re = /^[a-zA-Z]{2,10}$/i;
+ 
+    if (!re.test(name.value)) {
+        name.classList.add('is-invalid');
+    } else {
+        name.classList.remove('is-invalid');
+    }
+}
 
-// validateEmail() {
-//   let email = document.getElementById('email');
-//   // let re = /^([a-zA-Z_\-\.]+)@([a-zA-Z_\-\.]+)\.([a-zA-Z]{2,5})$/;
-//   let re = /^([\w\-\.]+)@([\w\-\.]+)\.([a-zA-Z]{2,5})$/;
+validateMessage() {
+  let message = document.getElementById('message');
+  console.log(message);
+  
+  let re = /^[a-zA-Z]{2,10}$/i;
 
-//   if (!re.test(email.value)) {
-//       email.classList.add('is-invalid');
-//   } else {
-//       email.classList.remove('is-invalid');
-//   }
-// }
-// }
+  if (!re.test(name.value)) {
+      name.classList.add('is-invalid');
+  } else {
+      name.classList.remove('is-invalid');
+  }
+}
 
+}
 
-alert ('Hello from Sendmail.js' )
