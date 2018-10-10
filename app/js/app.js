2144a2ee2b8dbe0629cbdf23d07ba09b9ff27aea
volosgoto <volosgoto@yahoo.com>
2018-10-10 10:40:34 +0300

    add qr

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,12 @@
 // Accordion handling
-
 function accondionFunction(id) {
     var x = document.getElementById(id);
     if (x.className.indexOf("w3-show") == -1) {
         x.className += " w3-show";
-    } else { 
+    } else {
         x.className = x.className.replace(" w3-show", "");
     }
 }
+
+
+
