b3033d0ec25750525501aa91d9a7d57c736fe12b
andrey <volosgoto@yahoo.com>
2018-11-05 22:36:27 +0200

    upd download booklet

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,36 @@
 // Accordion handling
 function accondionFunction(id) {
-    var x = document.getElementById(id);
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
-    }
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
 }
 
-function homeLink () {
-    window.location.href = '/public';
+function homeLink() {
+  window.location.href = '/public';
 }
 
 
 function sendMail() {
-    window.location.href = "mailto:office@synproeng.com";
+  window.location.href = "mailto:office@synproeng.com";
 }
 
 
-    let file = document.querySelector('.booklet-download');
-    file.addEventListener('click', downloadFile);
-    function downloadFile() {
-        let result =  confirm('Do you download Booklet Synprotech_LLC ');
-        if (result) {
-            setTimeout(
-            () => { 
-            let url='http://synproeng.com/assets/booklet/Booklet_Synprotech_ LLC_nov_2018.pdf';    
-            window.open(url, 'Download')}, 2000);
-  
-    }
-}
-    
+let file = document.querySelector('.booklet-download');
+file.addEventListener('click', (e) => {
+  e.preventDefault();
+  let confirmMsg = 'Do you download Booklet Synprotech_LLC?';
+  let result = confirm(confirmMsg);
+  if (result) {
+    setTimeout(() => {
+      // let url = 'http://synproeng.com/assets/booklet/Booklet_Synprotech_ LLC_nov_2018.pdf';
+      let url = '/assets/booklet/Booklet_Synprotech_ LLC_nov_2018.pdf';
+      window.open(url, '_self')
+    }, 2000);
+  } else {
+    let alertMsg = 'Downloading canceled';
+    alert(alertMsg);
+  }
+});
