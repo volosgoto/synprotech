86928316de0ed29106135ef33be950049d8b89e6
volosgoto <volosgoto@yahoo.com>
2018-11-06 11:16:48 +0200

    upd accordion angles

--- public/js/app.js
+++ public/js/app.js
@@ -21,15 +21,12 @@ function sendMail() {
 let file = document.querySelector('.booklet-download');
 file.addEventListener('click', (e) => {
   e.preventDefault();
-  let confirmMsg = 'Do you download Booklet Synprotech_LLC?';
+  let confirmMsg = 'Do you want to download Booklet Synprotech_LLC?';
   let result = confirm(confirmMsg);
   if (result) {
     setTimeout(() => {
       let url = '/assets/booklet/Booklet_Synprotech_ LLC_nov_2018.pdf';
-      window.open(url, '_self');
-    }, 2000);
-  } else {
-    let alertMsg = 'Downloading canceled!';
-    alert(alertMsg);
+      window.open(url, '_blank');
+    }, 100);
   }
 });
