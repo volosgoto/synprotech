499092ab0121e770dc1777b7fbc0010cd838f67b
andrey <volosgoto@yahoo.com>
2018-11-10 14:37:10 +0200

    add html to layout

---
+++ web/js/app.js
@@ -0,0 +1,44 @@
+// Accordion handling
+function accondionFunction(id) {
+  var x = document.getElementById(id);
+  if (id == 'consulting' || id == 'design' || id == 'operation' || id == 'constructing') {
+    // console.log('x', x.previousElementSibling.firstChild.nextElementSibling.textContent);
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+      x.previousElementSibling.firstChild.nextElementSibling.innerHTML = "&#xf106;";
+    } else {
+      x.className = x.className.replace(" w3-show", "");
+      x.previousElementSibling.firstChild.nextElementSibling.innerHTML = "&#xf107;";
+    }
+  } else {
+      if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+      } else {
+        x.className = x.className.replace(" w3-show", "");
+      }
+    }
+}
+
+
+function homeLink() {
+  window.location.href = '/oldpublic';
+}
+
+
+function sendMail() {
+  window.location.href = "mailto:office@synproeng.com";
+}
+
+
+let file = document.querySelector('.booklet-download');
+file.addEventListener('click', (e) => {
+  e.preventDefault();
+  let confirmMsg = 'Do you want to download Booklet Synprotech_LLC?';
+  let result = confirm(confirmMsg);
+  if (result) {
+    setTimeout(() => {
+      let url = '/assets/booklet/Booklet_Synprotech_ LLC_nov_2018.pdf';
+      window.open(url, '_blank');
+    }, 100);
+  }
+});
