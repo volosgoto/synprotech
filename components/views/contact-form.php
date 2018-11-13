d264afc7d3b734e3fcc4f1668b48c69899cf94b0
volosgoto <volosgoto@yahoo.com>
2018-11-13 14:30:03 +0200

    add contact form

--- components/views/contact-form.php
+++ components/views/contact-form.php
@@ -16,30 +16,26 @@ AppAsset::register($this);
 ?>
 
 <section class="section-contact-form">
-<!--     <div class="w3-container w3-dark-gray w3-twothird w3-center">-->
     <div class="w3-dark-gray w3-twothird w3-center">
         <div class="w3-card-4">
             <div class="w3-container w3-dark-gray ">
                 <h2 class="w3-margin-top"><?= Html::encode($this->title) ?></h2>
             </div>
-
-    <?php if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
-
-    <div class="alert alert-success">
+        <?php if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
+        <div class="alert alert-success">
         Message has been send.
-    </div>
-
-    <?php else: ?>
-    <?php $form = ActiveForm::begin([
+        </div>
+        <?php else: ?>
+        <?php $form = ActiveForm::begin([
         'id' => 'email-form',
         'options' => ['class' => 'w3-container w3-padding-xlarge'], /* класс формы */
         'action' => Url::home(),
         'method' => 'post',
         'fieldConfig' => [ /* классы полей формы */
-//            'template' => "<div class=\"col-lg-3\">{label}</div>\n<div class=\"col-lg-9\">{input}</div>\n<div class=\"col-lg-12 col-lg-offset-3 \">{error}</div>"
+        //            'template' => "<div class=\"col-lg-3\">{label}</div>\n<div class=\"col-lg-9\">{input}</div>\n<div class=\"col-lg-12 col-lg-offset-3 \">{error}</div>"
 
         ],
-    ]); ?>
+        ]); ?>
         <?= $form->field($model, 'name') ?>
         <?= $form->field($model, 'email') ?>
         <?= $form->field($model, 'subject') ?>
@@ -50,53 +46,14 @@ AppAsset::register($this);
                                 <div class="w3-input">{input}</div>
                             </div>',
         ]) ?>
-
         <div class="form-group">
             <?= Html::submitButton('Submit', ['id' => 'submit', 'class' => 'w3-button w3-black w3-large w3-hover-blue-gray', 'name' => 'contact-button']) ?>
         </div>
         <?php ActiveForm::end(); ?>
 
-    <?php endif; ?>
-    </div>
-
+        <?php endif; ?>
+        </div>
     </div>
 </section>
-
-    <!-- main contact form -->
-<!--<section class="section-contact-form">-->
-<!--    <!-- <div class="w3-container w3-dark-gray w3-twothird w3-center"> -->-->
-<!--    <div class="w3-dark-gray w3-twothird w3-center">-->
-<!--        <div class="w3-card-4">-->
-<!--            <div class="w3-container w3-dark-gray ">-->
-<!--                <h2 class="w3-margin-top">Contact Us</h2>-->
-<!--            </div>-->
-<!--            <form id="email-form" action="" class="w3-container w3-padding-xlarge" method="">-->
-<!--                <label class="w3-input ">Name</label>-->
-<!--                <input id="name" name="name" class="w3-input" type="text">-->
-<!--                <div class="invalid-feedback">-->
-<!--                    Enter name-->
-<!--                </div>-->
-<!--                <label class="w3-input">Email</label>-->
-<!--                <input id="email" name="email" class="w3-input" type="text">-->
-<!--                <div class="invalid-feedback">-->
-<!--                    Enter a valid Email-->
-<!--                </div>-->
-<!--                <label class="w3-input">Phone</label>-->
-<!--                <input id="phone" name="phone" class="w3-input" type="text">-->
-<!--                <div class="invalid-feedback">-->
-<!--                    Enter a valid phone-->
-<!--                </div>-->
-<!--                <label class="w3-input">Message </label>-->
-<!--                <textarea id="message" name="message" class="w3-input" style="resize:none"></textarea>-->
-<!--                <div class="invalid-feedback">-->
-<!--                    Enter text message-->
-<!--                </div>-->
-<!--                <br>-->
-<!--                <button onclick="sendMail()" id="submit" type="submit" class="w3-button w3-black w3-large w3-hover-blue-gray">Submit</button>-->
-<!--                <br><br>-->
-<!--            </form>-->
-<!--        </div>-->
-<!--</section>-->
-<!-- main contact form ENDS-->
 
 
