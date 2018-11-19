4695bd5c07fa0b84fdb081e6844a4f458495e005
andrey <volosgoto@yahoo.com>
2018-11-19 20:03:49 +0200

    add styling to admin panel

--- models/User.php
+++ models/User.php
@@ -57,4 +57,65 @@ class User extends \yii\db\ActiveRecord
             'active_status' => 'Active Status',
         ];
     }
+
+    public static function findIdentity($id)
+    {
+        return static::findOne($id);
+    }
+    /**
+     * @inheritdoc
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+//        return static::findOne(['access_token' => $token]);
+    }
+    /**
+     * Finds user by username
+     *
+     * @param  string      $username
+     * @return static|null
+     */
+    public static function findByUsername($username)
+    {
+        return static::findOne(['username' => $username]);
+    }
+    /**
+     * @inheritdoc
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * @inheritdoc
+     */
+    public function getAuthKey()
+    {
+        return $this->auth_key;
+    }
+    /**
+     * @inheritdoc
+     */
+    public function validateAuthKey($authKey)
+    {
+        return $this->auth_key === $authKey;
+    }
+    /**
+     * Validates password
+     *
+     * @param  string  $password password to validate
+     * @return boolean if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+//        return $this->password === $password;
+        return Yii::$app->security->validatePassword($password, $this->password);
+    }
+    public function generateAuthKey(){
+        $this->auth_key = Yii::$app->security->generateRandomString();
+    }
+    public function setPassword($password){
+        $passwordToHash = Yii::$app->security->generatePasswordHash($password);
+        return $this->password = $passwordToHash;
+    }
 }
